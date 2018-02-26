5d785e3caed8453db55f5c05354bc7b81d0ec13f
Raj Rathore <Raj.Rathore@tatacommunications.com>
2018-02-26 09:37:49 +0530

    Added a test suite

---
+++ src/test/java/DefaultTestSuite.java
@@ -0,0 +1,13 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Created by Raj Rathore on 24-Feb-18
+ */
+@RunWith(Suite.class)
+@SuiteClasses({StringHelperParameterizedComparisionTest.class,
+    StringHelperParameterizedTruncateTest.class})
+public class DefaultTestSuite {
+
+}
